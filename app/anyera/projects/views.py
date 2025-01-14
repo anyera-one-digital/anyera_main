425de05e0307840ed24aed17cd5c2755cde1b36c
OlegKot746 <oleg2102413@mail.ru>
2025-01-14 16:44:53 +0300

    Частичная реализация проекта

--- app/anyera/projects/views.py
+++ app/anyera/projects/views.py
@@ -9,7 +9,8 @@ def project_list(request):
 
 def project_detail(request, id):
     project = Project.objects.get(id=id)
-    return render(request, 'project.html', {'project': project})
+    other_projects = Project.objects.exclude(id=id)
+    return render(request, 'project.html', {'project': project, 'other_projects': other_projects})
 
 
 class ProjectViewSet(
