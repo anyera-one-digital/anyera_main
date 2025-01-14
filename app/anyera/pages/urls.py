5db5470aa349f9ea791f3d2aee5e6a7ff4630c29
NikAfraim <NikSen09@mail.ru>
2025-01-14 14:12:18 +0400

    code improvement

--- app/anyera/pages/urls.py
+++ app/anyera/pages/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from pages.views import main_page
+from pages.views import main_page, contacts_page
 
 urlpatterns = [
     path('', main_page, name='main_page'),
+    path('contacts/', contacts_page, name='contacts')
 ]
--- app/anyera/pages/views.py
+++ app/anyera/pages/views.py
@@ -3,9 +3,12 @@ from projects.models import Project
 from articles.models import Article
 
 def main_page(request):
-    projects = Project.objects.filter(is_visible=True)
+    projects = Project.objects.filter(select_on_main=True)
     articles = Article.objects.all()
     return render(request, 'index.html', {
         'projects': projects,
         'articles': articles
     })
+
+def contacts_page(request):
+    return render(request, 'contacts.html')
