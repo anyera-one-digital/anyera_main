9fe3762da6525207ef540344c7f8e5ae0f733df4
NikAfraim <NikSen09@mail.ru>
2025-01-15 09:26:32 +0400

    sliders

--- app/anyera/projects/views.py
+++ app/anyera/projects/views.py
@@ -1,7 +1,14 @@
 from rest_framework import mixins, viewsets
+from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import render
-from projects.models import Project
+from projects.models import Project, Slider
 from projects.serializers import ProjectSerializer, ProjectsListSerializer
+
+def get_object_or_none(model, **kwargs):
+    try:
+        return model.objects.get(**kwargs)
+    except ObjectDoesNotExist:
+        return None
 
 def project_list(request):
     projects = Project.objects.filter(is_visible=True)
@@ -10,7 +17,39 @@ def project_list(request):
 def project_detail(request, id):
     project = Project.objects.get(id=id)
     other_projects = Project.objects.exclude(id=id)
-    return render(request, 'project.html', {'project': project, 'other_projects': other_projects})
+
+    mobile_sliders = get_object_or_none(
+        Slider,
+        project=project,
+        type=Slider.SliderType.MOBILE
+    )
+
+    desktop_sliders = get_object_or_none(
+        Slider,
+        project=project,
+        type=Slider.SliderType.DESKTOP
+    )
+
+    tablet_sliders = get_object_or_none(
+        Slider,
+        project=project,
+        type=Slider.SliderType.TABLET
+    )
+
+    presentation_sliders = get_object_or_none(
+        Slider,
+        project=project,
+        type=Slider.SliderType.PRESENTATION
+    )
+
+    return render(request, 'project.html', {
+        'project': project,
+        'other_projects': other_projects,
+        'mobile_sliders': mobile_sliders,
+        'desktop_sliders': desktop_sliders,
+        'tablet_sliders': tablet_sliders,
+        'presentation_sliders': presentation_sliders,
+    })
 
 
 class ProjectViewSet(
