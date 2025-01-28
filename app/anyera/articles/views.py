bd2036f101a56c1097706f3b17eeab8663af335d
NikAfraim <NikSen09@mail.ru>
2025-01-28 12:09:12 +0400

    pagination

--- app/anyera/articles/views.py
+++ app/anyera/articles/views.py
@@ -1,5 +1,6 @@
 from rest_framework import mixins, viewsets
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from django.db.models import Count
 from articles.models import Article, ContentBlock, Theme
 from articles.serializers import ArticleSerializer, ArticleListSerializer
@@ -12,13 +13,27 @@ def article_list(request):
         Theme.objects.annotate(article_count=Count('article'))
         .filter(article_count__gt=0)
     )
-    return render(request, 'articles.html', {'articles': articles, 'themes': themes})
+    
+    paginator = Paginator(articles, 8)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'articles.html', {
+        'articles': page_obj.object_list,
+        'themes': themes,
+        'page_obj': page_obj,
+        'total_pages': paginator.num_pages
+    })
 
 def article_detail(request, code):
     article = Article.objects.get(code=code)
     blocks = ContentBlock.objects.filter(article=article)
     other_articles = Article.objects.exclude(code=code)
-    return render(request, 'article.html', {'article': article, 'blocks' : blocks, "other_articles" : other_articles})
+    return render(request, 'article.html', {
+        'article': article,
+        'blocks' : blocks,
+        "other_articles" : other_articles
+    })
 
 
 class ArticleViewSet(
